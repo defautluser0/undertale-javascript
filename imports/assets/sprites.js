171ab2b9d5228e83fce63bf38d4c4b30265c677c
defautluser0 <defautluser0@defautluser0.xyz>
2025-07-05 20:44:45 +0200

    add sprites

---
+++ imports/assets/sprites.js
@@ -0,0 +1,15 @@
+const spr_introimage = {
+	path: '/spr/introimage/',
+	frameCount: 11, // starts from 0, counts up
+	xoffset: 0,
+	yoffset: 0,
+}
+
+const spr_pixblk = {
+	path: '/spr/pixblk/',
+	frameCount: 1,
+	xoffset: 0,
+	yoffset: 0,
+}
+
+export { spr_introimage, spr_pixblk };
