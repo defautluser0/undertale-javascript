68730b9973d0ae223945baa10275fbc601a0403d
defautluser0 <defautluser0@defautluser0.xyz>
2025-07-05 19:26:34 +0200

    update

--- obj/introimage/index.js
+++ obj/introimage/index.js
@@ -1,124 +1,166 @@
-import { caster_load, caster_play, caster_stop, caster_free, caster_set_volume } from '/imports/customFunctions.js';
-import { mus_story } from '/imports/assets.js';
-import { instance_create, instance_destroy } from '/imports/assets/gamemakerFunctions.js';
-import global from '/imports/assets/global.js'
+import {
+  caster_load,
+  caster_play,
+  caster_stop,
+  caster_free,
+  caster_set_volume,
+} from "/imports/customFunctions.js";
+import { mus_story } from "/imports/assets.js";
+import {
+  instance_create,
+  instance_destroy,
+  instance_exists,
+} from "/imports/assets/gamemakerFunctions.js";
+import global from "/imports/assets/global.js";
+import { control_check_pressed } from "/imports/input.js";
 
-// general gamemaker variables
-let image_index = 0;
-let image_speed = 0;
-let image_number = 11;
-let visible = true;
-let x = 0;
-let y = 0;
-let instanceID = 0;
+/**
+ * Creates a new introimage instance with its own state.
+ */
+function create() {
+  return {
+    image_index: 0,
+    image_speed: 0,
+    image_number: 11,
+    visible: true,
+    x: 0,
+    y: 0,
+    instanceID: 0,
 
-// alarm setup
-const alarm = new Array(12).fill(-1);
-function updateGamemakerFunctions_introimage() {
-	for (let i = 0; i < alarm.lenght; i++) {
-		if (alarm[i] > 0) {
-			alarm[i]--;
-			if (alarm[i] === 0) {
-				globalThis[`alarm${i}`];
-			}
-		}
-	}
-	image_index += image_speed;
-	if (image_index >= image_number) {
-		image_index -=  image_number;
-	}
+    alarm: new Array(12).fill(-1),
+
+    act: 0,
+    skip: 0,
+    fadercreator: undefined,
+    vol: undefined,
+    dongs: undefined,
+
+    intromusic: caster_load(mus_story),
+  };
 }
 
-// ut code starts here
-let skip = 0;
-let act = 0;
-alarm[2] = 4;
-let intromusic = caster_load(mus_story);
-image_speed = 0;
-visible = false;
-let fadercreator = undefined;
-let vol = undefined;
-let dongs = undefined;
+/**
+ * Update alarms for this instance and call alarm handlers when timers reach 0.
+ */
+function updateGamemakerFunctions() {
+  for (let i = 0; i < this.alarm.length; i++) {
+    if (this.alarm[i] > 0) {
+      this.alarm[i]--;
+      if (this.alarm[i] === 0) {
+        const handler = this[`alarm${i}`];
+        if (typeof handler === "function") handler.call(this);
+      }
+    }
+  }
 
+  // Update image animation frame
+  this.image_index += this.image_speed;
+  if (this.image_index >= this.image_number) {
+    this.image_index -= this.image_number;
+  }
+}
+
+// Alarm 2 handler - start intro sequence
 function alarm2() {
-	act = 1;
-	dongs = 0;
-	image_speed = 0;
-	vol = 1;
-	caster_play(intromusic, 1, 0.91);
-	global.typer =  11;
-	global.faceemotion = 0;
-	global.msc = 0;
-	instance_create(0, 0, obj_introtangle);
-  fadercreator = 0;
-	skip = 0;
-	global.msg[0] = "Long ago^1, two races&ruled over Earth^1:&HUMANS and MONSTERS^6. \E1 ^1 %"
-	global.msg[1] = "One day^1, war broke&out between the two&races^6. \E0 ^1 %"
-	global.msg[2] = "After a long battle^1,&the humans were&victorious^6. \E1 ^1 %"
-	global.msg[3] = "They sealed the monsters&underground with a magic&spell^6. \E0 ^1 %"
-	global.msg[4] = "Many years later^2.^2.^5.  \E1 ^1 %"
-	global.msg[5] = "      MT. EBOTT&         201X^9 \E0 %"
-	global.msg[6] = "Legends say that those&who climb the mountain&never return^5.^3 \E1 %"
-	global.msg[7] = " \E1 %"
-	global.msg[8] = " ^9 ^5 \E0 %"
-	global.msg[9] = " ^9 ^5 ^2 \E1 %"
-	global.msg[10] = " ^9 ^5 ^2 \E2 %"
-	global.msg[11] = " ^9 ^9 ^9 ^9 ^9 ^9 \E2 %%"
-	global.msg[12] = " ^9 ^9 ^9 ^9 ^9  \E0 %%"
-	global.msg[13] = " ^9 ^9 ^9 ^9 ^9 ^9 \E0 %"
-	global.msg[14] = " %%"
-	let mywriter = instance_create(40, 140, OBJ_WRITER);
-	alarm[0] = 5;
+  this.act = 1;
+  this.dongs = 0;
+  this.image_speed = 0;
+  this.vol = 1;
+  caster_play(this.intromusic, 1, 0.91);
+  global.typer = 11;
+  global.faceemotion = 0;
+  global.msc = 0;
+  instance_create(0, 0, "obj_introtangle"); // pass object name as string or object reference
+  this.fadercreator = 0;
+  this.skip = 0;
+
+  global.msg[0] =
+    "Long ago^1, two races&ruled over Earth^1:&HUMANS and MONSTERS^6. \\E1 ^1 %";
+  global.msg[1] = "One day^1, war broke&out between the two&races^6. \\E0 ^1 %";
+  global.msg[2] =
+    "After a long battle^1,&the humans were&victorious^6. \\E1 ^1 %";
+  global.msg[3] =
+    "They sealed the monsters&underground with a magic&spell^6. \\E0 ^1 %";
+  global.msg[4] = "Many years later^2.^2.^5.  \\E1 ^1 %";
+  global.msg[5] = "      MT. EBOTT&         201X^9 \\E0 %";
+  global.msg[6] =
+    "Legends say that those&who climb the mountain&never return^5.^3 \\E1 %";
+  global.msg[7] = " \\E1 %";
+  global.msg[8] = " ^9 ^5 \\E0 %";
+  global.msg[9] = " ^9 ^5 ^2 \\E1 %";
+  global.msg[10] = " ^9 ^5 ^2 \\E2 %";
+  global.msg[11] = " ^9 ^9 ^9 ^9 ^9 ^9 \\E2 %%";
+  global.msg[12] = " ^9 ^9 ^9 ^9 ^9  \\E0 %%";
+  global.msg[13] = " ^9 ^9 ^9 ^9 ^9 ^9 \\E0 %";
+  global.msg[14] = " %%";
+
+  instance_create(40, 140, "OBJ_WRITER");
+  this.alarm[0] = 5;
 }
 
+// Alarm 1 handler - stop music and free sound
 function alarm1() {
-	caster_stop(intromusic);
-	caster_free(intromusic);
-	// room_goto_next()
+  caster_stop(this.intromusic);
+  caster_free(this.intromusic);
+  // room_goto_next() - implement your room transition here if needed
 }
 
+// Alarm 0 handler - handle fader creation and reset alarm 0
 function alarm0() {
-	if (fadercreator !== global.faceemotion) {
-		instance_create(0, 0, obj_introfader);
-	}
-	alarm[0] = 3
-	fadercreator = global.faceemotion;
+  if (this.fadercreator !== global.faceemotion) {
+    instance_create(0, 0, "obj_introfader");
+  }
+  this.alarm[0] = 3;
+  this.fadercreator = global.faceemotion;
 }
 
-function introimage_beginStep() {
-	if (act === 1) {
-		if (instance_exists(OBJ_WRITER) === 0 && skip === 0) {
-			skip = 1;
-			fader = instance_create(0, 0, obj_unfader);
-			fader.tspeed = 0.05;
-			alarm[1] = 30;
-		}
+/**
+ * Called every frame in the Begin Step event
+ */
+function beginStep() {
+  if (this.act === 1) {
+    if (!instance_exists("OBJ_WRITER") && this.skip === 0) {
+      this.skip = 1;
+      const fader = instance_create(0, 0, "obj_unfader");
+      fader.tspeed = 0.05;
+      this.alarm[1] = 30;
+    }
 
-		if (skip === 1) {
-			vol -= 0.05;
-			caster_set_volume(intromusic, vol);
-		}
+    if (this.skip === 1) {
+      this.vol -= 0.05;
+      caster_set_volume(this.intromusic, this.vol);
+    }
 
-		if (global.faceemotion == 2 && dongs == 0) {
-			dongs = 1;
-			instance_create()
-		}
-	}
+    if (global.faceemotion === 2 && this.dongs === 0) {
+      this.dongs = 1;
+      instance_create(/* pass appropriate params here */);
+    }
+  }
 }
 
-function introimage_step() {
-	if (act == 1) {
-		if (control_check_pressed(0)) {
-			if (skip == 0) {
-				skip = 1;
-				fader = innstance_create(0, 0, obj_unfader)
-				fader.tspeed = 0.05
-				alarm[1] = 30;
-
-				instance_destroy(OBJ_WRITER);
-			}
-		}
-	}
+/**
+ * Called every frame in the Step event
+ */
+function step() {
+  if (this.act === 1) {
+    if (control_check_pressed(0)) {
+      if (this.skip === 0) {
+        this.skip = 1;
+        const fader = instance_create(0, 0, "obj_unfader");
+        fader.tspeed = 0.05;
+        this.alarm[1] = 30;
+        instance_destroy("OBJ_WRITER");
+      }
+    }
+  }
 }
 
-export { introimage_beginStep, introimage_step, updateGamemakerFunctions_introimage, instanceID }
+export {
+  create,
+  updateGamemakerFunctions,
+  alarm0,
+  alarm1,
+  alarm2,
+  beginStep,
+  step,
+};
--- obj/soundtest/index.js
+++ obj/soundtest/index.js
@@ -28,16 +28,20 @@ import {
 } from "/imports/assets.js";
 
 // create
-let con = 0;
-let songno = 0;
-let gaster = 0;
-let g_timer = 0;
-let buffer = 0;
-let active_l = 0;
-let active_r = 0;
-let seeya_timer = 0;
+function create() {
+  return {
+    con: 0,
+    songno: 0,
+    gaster: 0,
+    g_timer: 0,
+    buffer: 0,
+    active_l: 0,
+    active_r: 0,
+    seeya_timer: 0,
+  };
+}
 
-function updateGamemakerFunctions_soundtest() {
+function updateGamemakerFunctions() {
   ogCtx.clearRect(0, 0, ogCanvas.width, ogCanvas.height);
   ogCtx.drawImage(
     canvas,
@@ -52,18 +56,18 @@ function updateGamemakerFunctions_soundtest() {
   );
 }
 
-function soundtest_step() {
+function step() {
   // step starts here
-  if (seeya_timer >= 180) {
+  if (this.seeya_timer >= 180) {
     room_goto("room_tundra3A");
   }
   // step ends ere
 }
 
-function soundtest_draw() {
+function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  // draw starts here
-  if (con === 0) {
+
+  if (this.con === 0) {
     draw_set_color(c_white);
     scr_setfont(fnt_main);
     scr_drawtext_icons_multiline(
@@ -72,27 +76,27 @@ function soundtest_draw() {
       "Welcome to the Sound Test!#Listen to all your favorites.#Press Left or Right to select.#Press *Z to play a song.#",
       2
     );
-    buffer = buffer + 1;
+    this.buffer++;
 
-    if (buffer >= 20) {
+    if (this.buffer >= 20) {
       if (control_check_pressed(0)) {
-        buffer = -10;
-        con = 1;
-        songno = 0;
+        this.buffer = -10;
+        this.con = 1;
+        this.songno = 0;
       }
     }
   }
 
-  if (con === 1) {
+  if (this.con === 1) {
     scr_setfont(fnt_main);
     draw_set_color(c_white);
     scr_drawtext_centered_scaled(320, 50, "SOUND TEST", 2, 2);
 
-    switch (songno) {
+    switch (this.songno) {
       case 0:
         if (audio_is_playing(mus_st_happytown)) {
           draw_set_color(c_yellow);
-        } else if (control_check_pressed(0) && gaster === 0) {
+        } else if (control_check_pressed(0) && this.gaster === 0) {
           audio_stop_all();
           audio_play_sound(mus_st_happytown, 50, true);
         }
@@ -102,7 +106,7 @@ function soundtest_draw() {
       case 1:
         if (audio_is_playing(mus_st_meatfactory)) {
           draw_set_color(c_yellow);
-        } else if (control_check_pressed(0) && gaster === 0) {
+        } else if (control_check_pressed(0) && this.gaster === 0) {
           audio_stop_all();
           audio_play_sound(mus_st_meatfactory, 50, true);
         }
@@ -112,7 +116,7 @@ function soundtest_draw() {
       case 2:
         if (audio_is_playing(mus_st_troubledingle)) {
           draw_set_color(c_yellow);
-        } else if (control_check_pressed(0) && gaster === 0) {
+        } else if (control_check_pressed(0) && this.gaster === 0) {
           audio_stop_all();
           audio_play_sound(mus_st_troubledingle, 50, true);
         }
@@ -122,8 +126,8 @@ function soundtest_draw() {
       case 3:
         if (audio_is_playing(mus_st_him)) {
           draw_set_color(c_yellow);
-        } else if (control_check_pressed(0) && gaster === 0) {
-          gaster = 1;
+        } else if (control_check_pressed(0) && this.gaster === 0) {
+          this.gaster = 1;
           audio_stop_all();
           audio_play_sound(mus_st_him, 50, true);
         }
@@ -131,59 +135,51 @@ function soundtest_draw() {
         break;
     }
 
-    buffer += 1;
+    this.buffer++;
 
-    if (keyboard_check_pressed(vk_left) && buffer > 0 && active_l < 0) {
-      if (gaster === 0) {
-        songno += 1;
-
-        if (songno > 3) songno = 0;
-
-        active_l = 10;
+    if (keyboard_check_pressed(vk_left) && this.buffer > 0 && this.active_l < 0) {
+      if (this.gaster === 0) {
+        this.songno++;
+        if (this.songno > 3) this.songno = 0;
+        this.active_l = 10;
       } else {
-        active_l = 2;
-        songno -= 1;
-        if (songno < 0) songno = 3;
+        this.active_l = 2;
+        this.songno--;
+        if (this.songno < 0) this.songno = 3;
       }
     }
 
-    if (keyboard_check_pressed(vk_right) && buffer > 0 && active_r < 0) {
-      active_r = 10;
-      songno = songno + 1;
-
-      if (songno > 3) songno = 0;
-
-      if (gaster === 1) active_r = 2;
+    if (keyboard_check_pressed(vk_right) && this.buffer > 0 && this.active_r < 0) {
+      this.active_r = 10;
+      this.songno++;
+      if (this.songno > 3) this.songno = 0;
+      if (this.gaster === 1) this.active_r = 2;
     }
 
-    active_l -= 1;
-    active_r -= 1;
+    this.active_l--;
+    this.active_r--;
+
     draw_set_color(c_white);
-    if (active_l > 0) {
-      draw_set_color(c_yellow);
-    }
+    if (this.active_l > 0) draw_set_color(c_yellow);
     draw_text(50, 200, "<<");
+
     draw_set_color(c_white);
-    if (active_r > 0) {
-      draw_set_color(c_yellow);
-    }
+    if (this.active_r > 0) draw_set_color(c_yellow);
     draw_text(580, 200, ">>");
 
-    if (gaster == 1) {
-      g_timer += 1;
-    }
+    if (this.gaster === 1) this.g_timer++;
 
-    if (g_timer >= 600) {
+    if (this.g_timer >= 600) {
       audio_stop_all();
-      con = 3;
+      this.con = 3;
     }
   }
-  if (con === 3) {
+
+  if (this.con === 3) {
     draw_text(150, 50, "Thanks for your feedback!");
     draw_text(150, 80, "Be seeing you soon!");
-    seeya_timer += 1;
+    this.seeya_timer++;
   }
-  // draw ends here
 }
 
-export { soundtest_step, soundtest_draw, updateGamemakerFunctions_soundtest };
+export { create, step, draw, updateGamemakerFunctions };
