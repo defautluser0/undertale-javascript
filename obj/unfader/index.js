a77d4d49616756811483a33cd8109150cb23f4ed
defautluser0 <defautluser0@defautluser0.xyz>
2025-07-05 20:45:18 +0200

    unfader

--- obj/unfader/index.js
+++ obj/unfader/index.js
@@ -1,14 +1,26 @@
+import { instance_destroy } from '/imports/assets/gamemakerFunctions.js'
+import roomSize from '/imports/assets/roomSize.js'
 
-function unfader_step() {
-	
+function create() {
+	return {
+		image_xscale: roomSize.width * 2,
+		image_yscale: roomSize.height,
+		image_alpha: 0,
+		tspeed: 0.08,
+		over: 0,
+	}
+}
+
+function step() {
+	this.image_alpha += this.tspeed;
 }
 
 function roomStart() {
-	
+	instance_destroy("obj_unfader");
 }
 
 function roomEnd() {
-	
+	instance_destroy("obj_unfader");
 }
 
-export { unfader_step, roomStart, roomEnd }
+export { create, step, roomStart, roomEnd }
