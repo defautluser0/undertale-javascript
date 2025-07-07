00344a188ba02bc6b2093a02702e37d6ee36f9d9
defautluser0 <defautluser0@defautluser0.xyz>
2025-07-07 14:51:52 +0200

    punching you with a card

--- imports/assets/sprites.js
+++ imports/assets/sprites.js
@@ -1,8 +1,8 @@
 const spr_introimage = {
 	path: '/spr/introimage/',
 	frameCount: 11, // starts from 0, counts up
-	xoffset: 160, // width / roomScale.width
-	yoffset: 120, // heigth / roomScale.height
+	xoffset: 160, // width / (ogCanvas.width / width)
+	yoffset: 120, // heigth / (ogCanvas.height / height)
 	width: 320,
 	height: 240,
 }
@@ -14,4 +14,13 @@ const spr_pixblk = {
 	yoffset: 0,
 }
 
-export { spr_introimage, spr_pixblk };
+const spr_punchcard = {
+	path: '/spr/punchcard/',
+	frameCount: 1,
+	xoffset: 160,
+	yoffset: 120,
+	width: 320,
+	height: 240,
+}
+
+export { spr_introimage, spr_pixblk, spr_punchcard };
