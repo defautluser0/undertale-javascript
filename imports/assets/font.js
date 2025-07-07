ec634294b18f58b5050b0168e104bfd9b56aad98
defautluser0 <defautluser0@defautluser0.xyz>
2025-07-07 03:57:19 +0200

    sand and origami (papiroflexia)

--- imports/assets/font.js
+++ imports/assets/font.js
@@ -3,5 +3,7 @@ const fnt_main = { name: "8bitOperatorPlus-Bold", size: 24 };
 const fnt_maintext = { name: "8bitOperatorPlus-Bold", size: 12 };
 const fnt_plain = { name: "DotumChe", size: 12 };
 const fnt_plainbig = { name: "DotumChe", size: 48 };
+const fnt_comicsans	= { name: "Comic Sans MS", size: 10 };
+const fnt_papyrus	= { name: "Papyrus", size: 8 };
 
-export { fnt_main, fnt_maintext, fnt_plain, fnt_plainbig }
+export { fnt_main, fnt_maintext, fnt_plain, fnt_plainbig, fnt_comicsans, fnt_papyrus }
