18db004e6b8a9efef8aecd64aace32f3f6d11045
defautluser0 <defautluser0@defautluser0.xyz>
2025-07-15 02:26:23 +0200

    doorA

--- obj/doorA/index.js
+++ obj/doorA/index.js
@@ -1,5 +1,7 @@
-import { draw_sprite_ext } from "/imports/assets/gamemakerFunctions.js";
+import { draw_sprite_ext, instance_create, room_goto, room_next, room_goto_next } from "/imports/assets/gamemakerFunctions.js";
 import { c_white } from "/imports/assets.js";
+import global from "/imports/assets/global.js"
+import * as obj_persistentfader from "/obj/persistentfader/index.js"
 
 function create() {
 	const alarm = new Array(12).fill(-1);
@@ -7,7 +9,7 @@ function create() {
 	// create code
 
 	return {
-		name: "objectname", // sprite name
+		name: "doorA", // sprite name
 		depth: 0, // object depth
 		image_xscale: 1, // sprite scale
 		image_yscale: 1, // sprite scale
@@ -17,17 +19,20 @@ function create() {
 		image_index: 0, // sprite frame index
 		image_speed: 0, // sprite frame speed
 		image_number: 0, // sprite frame number
-		sprite_index: null, // sprite object
-		visible: true, // sprite visibility
+		sprite_index: "spr_doorA", // sprite object
+		visible: false, // sprite visibility
 
 		alarm: alarm, // alarm array
 
 		// any variables assigned inside create code
+		touched: 0,
 
 		// object functions. add to here if you want them to be accessible from this. context
 		updateAlarms,
 		updateGamemakerFunctions,
 		updateSprite,
+		alarm2,
+		user9,
 	}
 }
 
@@ -53,7 +58,32 @@ function updateGamemakerFunctions() {
 }
 
 function updateSprite() {
-	draw_sprite_ext(this.sprite_index, this.image_index, this.x, this.y, this.image_xscale, this.image_yscale, 0, c_white, this.image_alpha)
+	if (this.visible === true) {
+		draw_sprite_ext(this.sprite_index, this.image_index, this.x, this.y, this.image_xscale, this.image_yscale, 0, c_white, this.image_alpha);
+	}
 }
 
-export { create, updateAlarms, updateGamemakerFunctions, updateSprite };
+function alarm2() {
+	global.entrance = 1;
+	instance_create(0, 0, obj_persistentfader)
+
+	if (global.currentRoom !== "room_castle_prebarrier") {
+		room_goto(room_next(global.currentRoom));
+	} else if (global.flag[7] === 1) {
+		room_goto("room_castle_trueexit");
+	} else {
+		room_goto_next();
+	}
+}
+
+function user9() {
+	global.interact = 3;
+	instance_create(0, 0, obj_unfader);
+
+	if (this.touched === 0) {
+		this.alarm[2] = 8;
+		touched = 1;
+	}
+}
+
+export { create, updateAlarms, updateGamemakerFunctions, updateSprite, alarm2, user9 };
