7f9adf96cac86c4f5609d8e90960a6c4a17a9d01
defautluser0 <defautluser0@defautluser0.xyz>
2025-07-27 03:54:55 +0200

    update template

--- obj/template/index.js
+++ obj/template/index.js
@@ -1,6 +1,7 @@
 import { draw_sprite_ext, getBoundingBox } from "/imports/assets/gamemakerFunctions.js";
 import { c_white } from "/imports/assets.js";
 
+import * as obj_solidobject from "/obj/solidobject/index.js"; // replace with a valid colliding object. if none, set it to null
 import * as parent from "/obj/parentobject/index.js"; // change as neccesary. if no parent, replace this line with "const parent = null;"
 
 function create() {
@@ -13,8 +14,6 @@ function create() {
     depth: 0, // object depth
     image_xscale: 1, // sprite scale
     image_yscale: 1, // sprite scale
-    x: 0, // object x. this is set by room
-    y: 0, // object y. this is set by room
     image_alpha: 1, // sprite alpha
     image_index: 0, // sprite frame index
     image_speed: 0, // sprite frame speed
@@ -38,12 +37,26 @@ function create() {
     updateAlarms,
     updateGamemakerFunctions,
     updateSprite,
+    updateCol,
+    followPath,
   };
   
   self._hspeed = 0;
   self._vspeed = 0;
   self._speed = 0;
   self._direction = 0;
+  self._path = {
+    data: {},
+    index: 1,
+    speed: 0,
+    endaction: "",
+    absolute: false,
+    xOffset: 0,
+    yOffset: 0,
+  }
+  self._x = 0;
+  self._y = 0;
+  self.initialspeed = null;
 
   Object.defineProperty(self, "hspeed", {
     get() {
@@ -85,6 +98,53 @@ function create() {
     },
   });
 
+  Object.defineProperty(self, "path_index", {
+    get() {
+      return this._path.data;
+    },
+    set(val) {
+      this._path.data = val;
+    }
+  })
+
+  Object.defineProperty(self, "path_speed", {
+    get() {
+      return this._path.speed;
+    },
+    set(val) {
+      this._path.speed = val;
+    }
+  })
+
+  Object.defineProperty(self, "path_endaction", {
+    get() {
+      return this._path.endaction;
+    },
+    set(val) {
+      this._path.endaction = val;
+    }
+  })
+
+  Object.defineProperty(self, "x", {
+    get() {
+      return this._x;
+    },
+    set(val) {
+      this._x = val
+      this._manualPos = true;
+    }
+  })
+
+  Object.defineProperty(self, "y", {
+    get() {
+      return this._y;
+    },
+    set(val) {
+      this._y = val
+      this._manualPos = true;
+    }
+  })
+
   self._updateCartesianFromPolar = function () {
     const rad = (this._direction * Math.PI) / 180;
     this._hspeed = Math.cos(rad) * this._speed;
@@ -121,31 +181,31 @@ function updateGamemakerFunctions() {
 		this.animationEnd?.();
   }
 
-  // getBoundingBox.call(this) // uncomment if bounding box is needed for something (collision checks from this or others)
+  getBoundingBox.call(this);
 
 	this.previousx = this.x;
 	this.xprevious = this.x;
 	this.previousy = this.y;
 	this.yprevious = this.y;
  
-  // Apply friction
+  // apply friction
   if (this.friction !== 0 && this.speed > 0) {
     this.speed -= this.friction;
     if (this.speed < 0) this.speed = 0;
   }
 
-  // Apply gravity vector
+  // apply gravity vector
   if (this.gravity) {
     let gravRad = this.gravity_direction * (Math.PI / 180);
     this.hspeed += Math.cos(gravRad) * this.gravity;
     this.vspeed -= Math.sin(gravRad) * this.gravity;
 
-    // Recalculate speed and direction based on new velocity
+    // recalculate speed and direction based on new velocity
     this.speed = Math.sqrt(this.hspeed * this.hspeed + this.vspeed * this.vspeed);
     this.direction = Math.atan2(-this.vspeed, this.hspeed) * (180 / Math.PI);
   }
 
-  // Update position
+  // update position
   this.x += this.hspeed;
   this.y += this.vspeed;
 }
@@ -171,4 +231,80 @@ function updateSprite() {
   }
 }
 
-export { create, updateAlarms, updateGamemakerFunctions, updateSprite, parent };
+function followPath() {
+  const pathState = this._path;
+  if (!pathState) return;
+
+  const points = pathState.data.points;
+  const keys = Object.keys(points).map(Number).sort((a, b) => a - b);
+
+  let currKey = pathState.index;
+  let nextKeyIndex = keys.indexOf(currKey) + 1;
+
+  if (nextKeyIndex >= keys.length) {
+    if (pathState.data.closed) {
+      nextKeyIndex = 0;
+    } else {
+      switch (pathState.endaction) {
+        case "path_action_stop":
+          this.x = points[currKey].x;
+          this.y = points[currKey].y;
+          this.speed = 0;
+          return;
+        case "path_action_loop":
+          pathState.index = keys[0];
+          nextKeyIndex = 1;
+          break;
+        default:
+          return;
+      }
+    }
+  }
+
+  const next = points[keys[nextKeyIndex]];
+
+  let unupdated = this._manualPos;
+
+  const dx = next.x - this.x;
+  const dy = next.y - this.y;
+  const dist = Math.hypot(dx, dy);
+
+  if (dist <= pathState.speed) {
+    this.x = next.x;
+    this.y = next.y;
+    if (unupdated) this._manualPos = false;
+    pathState.index = keys[nextKeyIndex];
+  } else {
+    this.x += (dx / dist) * pathState.speed;
+    this.y += (dy / dist) * pathState.speed;
+    if (unupdated) this._manualPos = false;
+  }
+
+  // apply GMS1.x-like direction update quirk
+  if (
+    this.initialspeed === 0 &&
+    this.path_speed > 0 &&
+    !this._manualVel &&
+    !this._manualPos
+  ) {
+    const radians = Math.atan2(-(this.y - this.yprevious), this.x - this.xprevious);
+    const degrees = (radians * 180) / Math.PI;
+    this.direction = (degrees + 360) % 360;
+  }
+}
+
+function updateCol() {
+  let other = collision_rectangle.call(this, this.bbox_left, this.bbox_top, this.bbox_right, this.bbox_bottom, obj_solidobject, false, false);
+  if (other) {
+    // collision updates with an object here. other
+    // is the colliding instance, so use 
+    // other.property for instance properties, like
+    // x, y and such.
+  }
+  // to add more collision checks, set other to 
+  // collision_rectangle.call(this, this.bbox_left, this.bbox_top, this.bbox_right, this.bbox_bottom, obj_solidobject2, false, false);, 
+  // obj_solidobject2 being a different solid object 
+  // and do another if (other) {} to run scripts.
+}
+
+export { create, updateAlarms, updateGamemakerFunctions, updateSprite, followPath, updateCol, parent };
