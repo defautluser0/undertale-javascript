a3070fde8037aa6cba792ffd9c5185fdff869c1b
defautluser0 <defautluser0@defautluser0.xyz>
2025-07-29 02:56:35 +0200

    prepare for room_ruins4

--- imports/assets/rooms.js
+++ imports/assets/rooms.js
@@ -1,6 +1,6 @@
 import global from "/imports/assets/global.js";
 
-const rooms = ["room_start" /* nonexistant, just so room 6 isnt area1_2 but ruins1. room actually exists in undertale */, "room_introstory", "room_introimage", "room_intromenu", "room_area1", "room_area1_2", "room_ruins1", "room_ruins2", "room_ruins3" ]
+const rooms = ["room_start" /* nonexistant, just so room 6 isnt area1_2 but ruins1. room actually exists in undertale */, "room_introstory", "room_introimage", "room_intromenu", "room_area1", "room_area1_2", "room_ruins1", "room_ruins2", "room_ruins3", "room_ruins4" ]
 
 if (global.debug === 1) {
 	globalThis.rooms = rooms;
